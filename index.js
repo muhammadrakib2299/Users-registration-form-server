7429e5c11b2e6d8f44d917c0c31ba5a52124e540
rakib2299 <mdrakib@combosoft.co.uk>
2025-09-25 19:25:49 +0600

    setup express and midlewire

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+// root - read only
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
