3a8a55a5ae5689e48c393073379d56d41015c1c2
rakib2299 <mdrakib@combosoft.co.uk>
2025-09-25 19:31:03 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewire
 app.use(cors());
 app.use(express.json());
+
+// Database => rakib --> rakib1234
+// mongodb connection url
+const uri =
+  "mongodb+srv://rakib:rakib1234@cluster0.bbzqc1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // root - read only
 app.get("/", (req, res) => {
